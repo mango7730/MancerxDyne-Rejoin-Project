af7cf32aee3e9f8a4d15a8851d9dfaed28974110
mango7730 <x.mancershop@gmail.com>
2026-09-05 18:07:44 +0700

    Update install script for runmadinefreetools

--- install.sh
+++ install.sh
@@ -6,15 +6,10 @@ rm -f /data/data/com.termux/files/usr/bin/mancer
 rm -f /data/data/com.termux/files/usr/bin/runmadinefreetools
 rm -f /data/data/com.termux/files/usr/bin/.madine_core
 
-curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/refs/heads/main/runmadinefreetools" -o /data/data/com.termux/files/usr/bin/.madine_core
-chmod +x /data/data/com.termux/files/usr/bin/.madine_core
+curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/main/runmadinefreetools%20(2).txt" -o runmadinefreetools
 
-cat << 'WRAPPER' > /data/data/com.termux/files/usr/bin/runmadinefreetools
-#!/bin/bash
-su -c /data/data/com.termux/files/usr/bin/.madine_core
-WRAPPER
-
-chmod +x /data/data/com.termux/files/usr/bin/runmadinefreetools
+chmod +x runmadinefreetools
+mv runmadinefreetools /data/data/com.termux/files/usr/bin/runmadinefreetools
 clear
 echo -e "\e[1;32m[+] Install Sukses!\e[0m"
 echo -e "\e[1;37mKetik perintah ini untuk mulai: \e[1;31mrunmadinefreetools\e[0m"
