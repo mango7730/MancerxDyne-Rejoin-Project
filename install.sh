f5cf99e7a88167c666e8ad39d4b65c6e127d5482
mango7730 <x.mancershop@gmail.com>
2026-09-05 17:55:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@ echo -e "\e[1;31m[*] Menginstall MADINE FREE TOOLS V1.0...\e[0m"
 
 rm -f /data/data/com.termux/files/usr/bin/mancer
 rm -f /data/data/com.termux/files/usr/bin/runmadinefreetools
+rm -f /data/data/com.termux/files/usr/bin/.madine_core
 
-curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/refs/heads/main/runmadinefreetools" -o runmadinefreetools
+curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/refs/heads/main/runmadinefreetools" -o /data/data/com.termux/files/usr/bin/.madine_core
+chmod +x /data/data/com.termux/files/usr/bin/.madine_core
 
-chmod +x runmadinefreetools
-mv runmadinefreetools /data/data/com.termux/files/usr/bin/runmadinefreetools
+cat << 'WRAPPER' > /data/data/com.termux/files/usr/bin/runmadinefreetools
+#!/bin/bash
+su -c /data/data/com.termux/files/usr/bin/.madine_core
+WRAPPER
+
+chmod +x /data/data/com.termux/files/usr/bin/runmadinefreetools
 clear
 echo -e "\e[1;32m[+] Install Sukses!\e[0m"
 echo -e "\e[1;37mKetik perintah ini untuk mulai: \e[1;31mrunmadinefreetools\e[0m"
