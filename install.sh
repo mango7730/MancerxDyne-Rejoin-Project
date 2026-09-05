79b5775a18a63e1ef5167a4a4b489ac9a21f7bd8
mango7730 <x.mancershop@gmail.com>
2026-09-06 01:04:18 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-clear
-echo -e "\e[1;31m[*] Menginstall MADINE FREE TOOLS V1.1...\e[0m"
-
-rm -f /data/data/com.termux/files/usr/bin/mancer
-rm -f /data/data/com.termux/files/usr/bin/runmadinefreetools
-rm -f /data/data/com.termux/files/usr/bin/.madine_core
-
-curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/main/runmadinefreetools.txt" -o runmadinefreetools
-
-chmod +x runmadinefreetools
-mv runmadinefreetools /data/data/com.termux/files/usr/bin/runmadinefreetools
-clear
-echo -e "\e[1;32m[+] Install Sukses!\e[0m"
-echo -e "\e[1;37mKetik perintah ini untuk mulai: \e[1;31mrunmadinefreetools\e[0m"
+curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/main/runmadinefreetools.txt" -o $PREFIX/bin/runmadinefreetools && chmod +x $PREFIX/bin/runmadinefreetools && clear && echo -e "\e[1;32m[✓] TOOLS INSTALLED SUCCESSFULLY!\e[0m\n\e[1;37mType \e[1;31mrunmadinefreetools\e[1;37m to start.\e[0m"
