36a40e14c9064793c592da6d947d9d49eba1d8d4
mango7730 <x.mancershop@gmail.com>
2026-09-05 17:43:31 +0700

    MADINE FREE TOOLS V1.0

--- install.sh
+++ install.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
-
 clear
-echo -e "\e[1;36m========================================\e[0m"
-echo -e "\e[1;37m      INSTALLING MANCER AUTO REJOIN     \e[0m"
-echo -e "\e[1;36m========================================\e[0m"
+echo -e "\e[1;31m[*] Menginstall MADINE FREE TOOLS V1.0...\e[0m"
 
-echo -e "\e[1;33m[*] Menyiapkan penyimpanan...\e[0m"
-termux-setup-storage
-sleep 2
+rm -f /data/data/com.termux/files/usr/bin/mancer
+rm -f /data/data/com.termux/files/usr/bin/runmadinefreetools
 
-echo -e "\e[1;33m[*] Mengunduh sistem inti...\e[0m"
-# Mendownload biner Mancer dari link raw lu
-curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/refs/heads/main/mancer" -o /data/data/com.termux/files/usr/bin/mancer
+curl -sL "https://github.com/mango7730/MancerxDyne-Rejoin-Project/raw/refs/heads/main/runmadinefreetools" -o runmadinefreetools
 
-echo -e "\e[1;33m[*] Mengatur izin akses...\e[0m"
-chmod +x /data/data/com.termux/files/usr/bin/mancer
-sleep 1
-
-echo -e "\e[1;32m[+] INSTALASI BERHASIL!\e[0m"
-echo -e "\e[1;37mMulai sekarang, lu cuma perlu ketik perintah:\e[0m"
-echo -e "\e[1;32mmancer\e[0m"
-echo -e "\e[1;37muntuk membuka tools ini kapan saja!\e[0m"
-echo -e "\e[1;36m========================================\e[0m"
-
-# Langsung membuka tools
-mancer
+chmod +x runmadinefreetools
+mv runmadinefreetools /data/data/com.termux/files/usr/bin/runmadinefreetools
+clear
+echo -e "\e[1;32m[+] Install Sukses!\e[0m"
+echo -e "\e[1;37mKetik perintah ini untuk mulai: \e[1;31mrunmadinefreetools\e[0m"
